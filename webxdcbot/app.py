3b6bb6a49032c1f2cc40fe65fb25801cf6058472
adbenitez <asieldbenitez@gmail.com>
2023-10-16 20:29:50 +0200

    use "info" property and link to webxdc specs

--- webxdcbot/app.py
+++ webxdcbot/app.py
@@ -1,6 +1,14 @@
+"""Example processing of incoming webxdc status updates."""
 from fortune import fortune
 
 
 def get_response(payload):
+    # only handle requests, ignore responses from self
     if payload.get("request"):
-        return {"payload": {"response": {"name": "bot", "text": fortune()}}}
+        text = fortune()
+        response = {"response": {"name": "bot", "text": text}}
+
+        # check https://docs.webxdc.org/spec.html#sendupdate
+        update = {"payload": response, "info": text}
+
+        return update
